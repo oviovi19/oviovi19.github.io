e7c5ea10f14f6a0518910dbf0de689ed7e670c4c
oviovi19 <165977917+oviovi19@users.noreply.github.com>
2024-10-05 11:30:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'biblico1',
+    'qual dicipulo cortou a orelha do soldado',
+    'pedro'
+)
+
+criaCartao(
+    'biblico2',
+    'quantos dicipulos Jesus escolheu',
+    '12 dicipulos'
+)
+
+criaCartao(
+    'biblico3',
+    'quem foi enviado por Deus a ninive',
+    'jonas'
+)
+
+criaCartao(
+    'biblico4',
+    'quem foi o jovem que virou escavo e depois governador do egito',
+    'jose'
+)
+
+criaCartao(
+    'biblico5',
+    'qual era a funcao de matheus antes de seguir Jesus',
+    'cobrador de impostos'
+)
+
+criaCartao(
+    'biblico6',
+    'onde Jesus foi crucificado',
+    'golgota'
+)
+
+criaCartao(
+    'biblico7',
+    'quem subiu numa figueira brava para ver Jesus',
+    'zaqueu'
+)
+
+criaCartao(
+    'biblico8',
+    'quais eram os sonhos que jose interpretou para farao',
+    'sete espigas boas e sete espigas murchas e sete vacas gordas e sete vacas magras'
+)
+
+criaCartao(
+    'biblico9',
+    'quem foi o homem mais velho da biblia',
+    'matusalem'
+)
+
+criaCartao(
+    'biblico10',
+    'quando sera a volta de Jesus',
+    'apenas Deus sabe, nem seus anjos, nem Jesus sabem. mas estao a porta, vigiai e orai, para nao cairem em tentacao.'
+)
